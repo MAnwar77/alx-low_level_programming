043eeb4f26a4ac9f5c69475d28ff9698ed54ac92
MAnwar77 <kenmka76@gmail.com>
2023-06-18 21:49:39 -0700

    first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - entry point
+ * main - Entry Point
  *
  * Description: A C program that prints with put function
  *
@@ -10,6 +10,6 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
