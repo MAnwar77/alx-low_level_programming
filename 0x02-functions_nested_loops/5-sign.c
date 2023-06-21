da5970bc869bd1083333fe72deab3bd9ffcf8117
MAnwar77 <kenmka76@gmail.com>
2023-06-21 15:33:12 -0700

    trd commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -14,17 +14,17 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		printf("+\n");
-			return (1);
+		_putchar(43);
+		return (1);
 	}
 	else if (n == 0)
 	{
-		printf("0\n");
-			return (0);
+		_putchar(48);
+		return (0);
 	}
 	else
 	{
-		printf("-\n");
-			return (-1);
+		_putchar(45);
+		return (-1);
 	}
 }
