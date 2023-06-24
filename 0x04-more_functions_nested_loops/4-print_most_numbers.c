c71ccbca3b760a2fd298df40527457dfcc8ef135
MAnwar77 <kenmka76@gmail.com>
2023-06-23 18:12:27 -0700

    ffth commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
-* print_most_numbers - function that prints the numbers, from 0 to 9 except 2 and 4
+* print_most_numbers - function that prints the numbers except 2 and 4
 */
 
 void print_most_numbers(void)
