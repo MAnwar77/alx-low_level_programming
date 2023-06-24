00f94c410e11694229834870d419a14e3a19cbcc
MAnwar77 <kenmka76@gmail.com>
2023-06-23 19:22:50 -0700

    ffth commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line
+ * @n: takes integer type input of the function
+*/
+
+void print_line(int n)
+{
+	int num_lines;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (num_lines = 1; num_lines <= n; num_lines++)
+		{
+			_putchar("\\");
+		}
+		_putchar('\n');
+	}
+}
