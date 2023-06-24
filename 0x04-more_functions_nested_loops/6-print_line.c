8b37af73867fc674aae66387129cb374a987f0ca
MAnwar77 <kenmka76@gmail.com>
2023-06-23 19:13:03 -0700

    ffth commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - function that draws a straight line
+ * @n: takes integer type input of the function
+*/
+
+void print_line(int n)
+{
+	int n;
+
+	if (n > 0)
+		_putchar(95);
+	_putchar('\n');
+	else
+		_putchar('\n');
+}
