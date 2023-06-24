1f1ac7dbffc42689a5d7bc97005d505cbd135dab
MAnwar77 <kenmka76@gmail.com>
2023-06-23 18:04:12 -0700

    ffth commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_numbers - function that prints the numbers, from 0 to 9 except 2 and 4
+*/
+
+void print_numbers(void)
+{
+	int ch;
+
+	for (ch = 48; ch <= 57; ch++)
+	{
+		for (ch == 50 || ch == 52)
+			continue;
+		_putchar(ch);
+	}
+	_putchar('\n');
+}
